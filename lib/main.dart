dd1ddf9ddf26f6ca7d6f10d97d7cdb03fd49687b
akash580703 <80091998+akash580703@users.noreply.github.com>
2022-07-16 12:24:44 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget{
  Widget build(BuildContext context){
    
    return MaterialApp(
-    home:RandomWord(),
+//     home:RandomWord(),
     theme:ThemeData(primaryColor:Colors.red[200])  
    );
 
